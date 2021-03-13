db1edaa0db2fec80ed17495e5c0967ff179d19a3
Diego Rufino <diegorufino1@gmail.com>
2021-03-12 21:31:07 -0300

    Criacao de aplicação em react, integrado com api

---
+++ src/views/UserList/components/UsersToolbar/UsersToolbar.js
@@ -0,0 +1,96 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import clsx from 'clsx';
+import { makeStyles } from '@material-ui/styles';
+import { 
+  Button,
+  TextField,
+  Grid,
+  Select,
+  MenuItem,
+  FormControl,
+  InputLabel,
+  Menu
+} from '@material-ui/core';
+
+const useStyles = makeStyles(theme => ({
+  root: {},
+  row: {
+    height: '42px',
+    display: 'flex',
+    alignItems: 'center',
+    marginTop: theme.spacing(1)
+  },
+  spacer: {
+    flexGrow: 1
+  },
+  importButton: {
+    marginRight: theme.spacing(1)
+  },
+  exportButton: {
+    marginRight: theme.spacing(1)
+  },
+  searchInput: {
+    marginRight: theme.spacing(1)
+  }
+}));
+
+const UsersToolbar = props => {
+  const { className, ...rest } = props;
+
+  const [descricao, setDescricao] = useState('')
+  const [categoria, setCategoria] = useState('')
+
+  const classes = useStyles();
+
+  const submit = (event) => {
+    event.preventDefault(); //quando der o enteder nao submiter o formulario
+    console.log(`Valores: Descrição - ${descricao}, Categoria - ${categoria}`)
+  }
+
+  return (
+    <div
+      {...rest}
+      className={clsx(classes.root, className)}
+    >
+      <div className={classes.row}>
+        <span className={classes.spacer} />
+
+      </div>
+      <div className={classes.row}>
+        <Grid container>
+          <Grid item md={4}>
+            <TextField
+              className={classes.searchInput}
+              placeholder="Descrição da tarefa"
+              label="Descrição:"
+              fullWidth
+              value={descricao}
+              onChange={e => setDescricao(e.target.value)}
+            />
+          </Grid>
+          <Grid item md={4}>
+            <FormControl fullWidth>
+              <InputLabel>Categoria: </InputLabel>
+              <Select value={categoria} onChange={e => setCategoria(e.target.value)}>
+              <MenuItem value="">Selecione...</MenuItem>
+                <MenuItem value={"TRABALHO"}>Trabalho</MenuItem>
+                <MenuItem value={"ESTUDO"}>Estudo</MenuItem>
+                <MenuItem value={"OUTROS"}>Outros</MenuItem>
+              </Select>
+            </FormControl>
+          </Grid>
+          <Grid item md={2}>
+            <Button onClick={submit} variant="contained" color="secondary">Adicionar</Button>
+          </Grid>
+        </Grid>
+      </div>
+    </div>
+  );
+};
+
+UsersToolbar.propTypes = {
+  className: PropTypes.string
+};
+
+export default UsersToolbar;
