f2f80fd82c827646ec72ed80f76df7979059dd3c
Diego Rufino <diegorufino1@gmail.com>
2021-03-16 13:48:26 -0300

    Instalando e configurando o Redux

--- src/index.js
+++ src/index.js
@@ -4,6 +4,20 @@ import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import mainReducer from './store'
+
+// toda vez que for despachar uma action, precisa do objeto dispatch, 
+// com o redux-thunk eu despacho quantas actions eu precisar
+// store - é o conjunto de reducers que vai ser criado no mainReducer
+const store = applyMiddleware(thunk)(createStore)(mainReducer)
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+, document.getElementById('root'));
 
 serviceWorker.unregister();
